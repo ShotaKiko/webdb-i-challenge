2c7103c99f800ef24734e82f5419b224cd0596b0
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-22 21:49:07 -0500

    Added read/get endpoint

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
 
 const server = express();
+const db = require('./data/accounts-model.js')
 
-// your code here
+//Middleware
+server.use(express.json())
+
+// Routing
+server.get('/', (req, res) => {
+    res.send(`
+    <h2>
+        WEBDB1 
+    </h2>`)
+})
+
+server.get('/api/accounts', async (req, res) => {
+    try{
+        const accountList = await db.find();
+        res.status(200).json(accountList)
+    } catch (error) {
+        res.status(500).json({
+            message: 'The account list could not be retrieved.'
+        })
+    }
+})
 
 module.exports = server;
